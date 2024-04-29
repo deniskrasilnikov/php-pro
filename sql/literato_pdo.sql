8d34eb58d5bab1de8e11c8b77440c6b9ce78d3b4
Denis Krasilnikov <deniskrasilnikov86@gmail.com>
2024-04-29 22:00:15 +0300

    literato db schema sql

---
+++ sql/literato_pdo.sql
@@ -0,0 +1,47 @@
+create table author
+(
+    id         int unsigned auto_increment
+        primary key,
+    first_name varchar(50) not null,
+    last_name  varchar(50) not null
+);
+
+create table book
+(
+    id        int unsigned auto_increment
+        primary key,
+    name      varchar(50)                                                                              not null,
+    isbn      char(13)                                                                                 not null,
+    text      text                                                                                     null,
+    author_id int unsigned                                                                             not null,
+    genres    set ('Adventure', 'Science fiction', 'Thriller', 'Horror', 'Magical realism', 'Romance') null,
+    constraint book_ibfk_1
+        foreign key (author_id) references author (id)
+);
+
+create index author_id
+    on book (author_id);
+
+create table publisher
+(
+    id      int unsigned auto_increment
+        primary key,
+    name    varchar(200) not null,
+    address varchar(500) not null
+);
+
+create table author_publisher
+(
+    author_id    int unsigned not null,
+    publisher_id int unsigned not null,
+    constraint author_id
+        unique (author_id, publisher_id),
+    constraint author_publisher_ibfk_1
+        foreign key (author_id) references author (id),
+    constraint author_publisher_ibfk_2
+        foreign key (publisher_id) references publisher (id)
+);
+
+create index publisher_id
+    on author_publisher (publisher_id);
+
