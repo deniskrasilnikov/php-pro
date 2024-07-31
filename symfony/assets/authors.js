98b76c743ac0ce53c3e6a80489bf7573651133bb
Denis Krasilnikov <deniskrasilnikov86@gmail.com>
2024-07-31 12:35:38 +0300

    fix js error for authors page

--- symfony/assets/authors.js
+++ symfony/assets/authors.js
@@ -1,27 +1,29 @@
 // скрипти для сторінки авторів
 
-const authorsHeader = document.getElementById('authors-from-json');
-authorsHeader.innerHTML = '<a href="#">Click to get authors from JSON</a>';
+(function () {
+    const authorsHeader = document.getElementById('authors-from-json');
+    authorsHeader.innerHTML = '<a href="#">Click to get authors from JSON</a>';
 
-authorsHeader.onclick = function (event) {
-    console.log(event)
-    renderAuthorsFromJSON();
-}
+    authorsHeader.onclick = function (event) {
+        console.log(event)
+        renderAuthorsFromJSON();
+    }
 
-function renderAuthorsFromJSON() {
-    fetch('/authors.json', {
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
-        .then(response => response.json())
-        .then(function (authors) {
-            console.log(authors);
-            let html = '';
-            for (let i = 0; i < authors.length; i++) {
-                let author = authors[i];
-                html += `<div>${author.firstName} ${author.lastName}</div>`;
+    function renderAuthorsFromJSON() {
+        fetch('/authors.json', {
+            headers: {
+                "Content-Type": "application/json",
             }
-            document.getElementById('authors-from-json').innerHTML = html;
         })
-}
+            .then(response => response.json())
+            .then(function (authors) {
+                console.log(authors);
+                let html = '';
+                for (let i = 0; i < authors.length; i++) {
+                    let author = authors[i];
+                    html += `<div>${author.firstName} ${author.lastName}</div>`;
+                }
+                document.getElementById('authors-from-json').innerHTML = html;
+            })
+    }
+})()
